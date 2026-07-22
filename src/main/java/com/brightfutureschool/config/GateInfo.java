779080d4158565361c3b4e565a3db05b241eec56
usmanbukhari90 <mrbukhari900@gmail.com>
2026-07-22 12:29:21 +0500

    Add GateInfo model class

---
+++ src/main/java/com/brightfutureschool/config/GateInfo.java
@@ -0,0 +1,11 @@
+package com.brightfutureschool.config;
+
+public class GateInfo {
+    public static final String WHATSAPP_NUMBER = "923183074951"; // country code + number, no + or spaces
+    public static final String EMAIL = "gatedevelopers.pk@gmail.com";
+    public static final String FACEBOOK_URL = "https://www.facebook.com/syed.usman.bukhari.595612";
+    public static final String INSTAGRAM_URL = "https://www.instagram.com/iusmanbukhari_";
+    public static final String LINKEDIN_URL = "https://www.linkedin.com/in/usman-bukhari-17b1443a7";
+    public static final String TWITTER_URL = "https://x.com/iusmanbukhari_";
+    public static final String GITHUB_URL = "https://github.com/usmanbukhari90"; // TODO: replace with your actual GitHub URL
+}
