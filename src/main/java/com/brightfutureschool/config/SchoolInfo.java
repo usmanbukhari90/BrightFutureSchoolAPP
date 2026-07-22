cd967ea01e9b1cf1de8e35b35c5ee487eacae824
usmanbukhari90 <mrbukhari900@gmail.com>
2026-07-22 12:28:59 +0500

    Add SchoolInfo model class

---
+++ src/main/java/com/brightfutureschool/config/SchoolInfo.java
@@ -0,0 +1,8 @@
+package com.brightfutureschool.config;
+
+public class SchoolInfo {
+    public static final String SCHOOL_NAME = "Bright Future School";
+    public static final String ADDRESS = "Muslim Town, Manka Canal Road, D.G. Khan";
+    public static final String PHONE_1 = "0333-7253940";
+    public static final String PHONE_2 = "0313-7254087";
+}
